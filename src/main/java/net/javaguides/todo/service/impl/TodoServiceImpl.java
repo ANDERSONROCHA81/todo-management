0563ea05545bc1fe17206c604c678788f2c7b4df
andersonrocha <ironmanrocha81@gmail.com>
2024-02-03 14:30:34 -0300

    Criação das classes TodoController, TodoService e TodoServiceImpl com seus respectivos métodos

---
+++ src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
@@ -0,0 +1,36 @@
+package net.javaguides.todo.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.todo.dto.TodoDto;
+import net.javaguides.todo.entity.Todo;
+import net.javaguides.todo.repository.TodoRepository;
+import net.javaguides.todo.service.TodoService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TodoServiceImpl implements TodoService {
+
+    private TodoRepository todoRepository;
+    @Override
+    public TodoDto addTodo(TodoDto todoDto) {
+
+        //Convert TodoDto into Todo Jpa entity
+        Todo todo = new Todo();
+        todo.setTitle(todoDto.getTitle());
+        todo.setDescription(todoDto.getDescription());
+        todo.setCompleted(todo.isCompleted());
+
+        //Todo jpa entity
+        Todo savedTodo = todoRepository.save(todo);
+
+        //Convert saved Todo Jpa entity object into TodoDto object
+        TodoDto savedTodoDto = new TodoDto();
+        savedTodoDto.setId(savedTodo.getId());
+        savedTodoDto.setTitle(savedTodo.getTitle());
+        savedTodoDto.setDescription(savedTodo.getDescription());
+        savedTodoDto.setCompleted(savedTodo.isCompleted());
+
+        return savedTodoDto;
+    }
+}
