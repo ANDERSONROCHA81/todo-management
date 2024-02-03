b48a0bf99c5ab0cf3d9c3e0d4293d80d2f11c834
andersonrocha <ironmanrocha81@gmail.com>
2024-02-03 15:19:47 -0300

    Adição das descrições dos métodos para o springDoc

--- src/main/java/net/javaguides/todo/TodoManagementApplication.java
+++ src/main/java/net/javaguides/todo/TodoManagementApplication.java
@@ -1,9 +1,27 @@
 package net.javaguides.todo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Todo Management REST API Documentation",
+				description = "Spring Boot Todo Management REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Anderson",
+						email = "ironmanrocha81@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		)
+)
 public class TodoManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/net/javaguides/todo/controller/TodoController.java
+++ src/main/java/net/javaguides/todo/controller/TodoController.java
@@ -1,5 +1,8 @@
 package net.javaguides.todo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import net.javaguides.todo.dto.TodoDto;
 import net.javaguides.todo.service.TodoService;
@@ -10,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "CRUD REST APIs for Todo Resource",
+        description = "CRUD REST APIs - Add Todo, Update Todo, Get Todo, Get All Todos, Delete Todo"
+)
 @RestController
 @RequestMapping("api/todos")
 @AllArgsConstructor
@@ -17,6 +24,14 @@ public class TodoController {
 
     private TodoService todoService;
 
+    @Operation(
+            summary = "Add Todo REST API",
+            description = "Add Todo REST API is used to save a todo in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     //Build Add Todo REST API
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
