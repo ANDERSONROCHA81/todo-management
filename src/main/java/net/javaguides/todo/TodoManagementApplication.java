29e414137c9e30267208ed55f6b6e1b9686e6eab
andersonrocha <ironmanrocha81@gmail.com>
2024-02-05 11:27:48 -0300

    Adição da dependência ModelMapper para mapeamento entre classes

--- src/main/java/net/javaguides/todo/TodoManagementApplication.java
+++ src/main/java/net/javaguides/todo/TodoManagementApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -24,6 +26,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 )
 public class TodoManagementApplication {
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TodoManagementApplication.class, args);
 	}
--- src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import net.javaguides.todo.dto.TodoDto;
 import net.javaguides.todo.entity.Todo;
 import net.javaguides.todo.repository.TodoRepository;
 import net.javaguides.todo.service.TodoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,25 +13,18 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+
+    private ModelMapper modelMapper;
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
         //Convert TodoDto into Todo Jpa entity
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todo.isCompleted());
+        Todo todo = modelMapper.map(todoDto, Todo.class);
 
         //Todo jpa entity
         Todo savedTodo = todoRepository.save(todo);
-
         //Convert saved Todo Jpa entity object into TodoDto object
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
 
-        return savedTodoDto;
+        return modelMapper.map(savedTodo, TodoDto.class);
     }
 }
